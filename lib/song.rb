1dde872b42d939659d1f8dec0d3e2b9665bca3d3
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-31 21:56:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,37 @@
 class Song
+
   attr_accessor :name, :artist, :genre
+
   @@count = 0
   @@genres = []
+  @@artists = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
     @genre << @@genres
+    @artist << @@artists
   end
+
   def count
     @@count
   end
+
   def genres
     gens = []
     @@genres.each do |gen|
       gen << gens if gens.include?(gen) == false
     end
-    gens 
+    gens
+  end
+
+  def artists
+    arts = []
+    @@artists.each do |art|
+      art << arts if arts.include?(art) == false
+    end
+    arts
   end
 end
